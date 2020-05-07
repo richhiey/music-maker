207b8b8882d31853c1ef9b36510ff028c8a3b08a
Richhiey Thomas <richhiey.thomas@gmail.com>
2020-05-07 19:26:43 +0200

    Remove rest_framework package

--- music_canvas/urls.py
+++ music_canvas/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 from apps.interface import views as interface_views 
 from django.conf.urls.static import static
 from django.conf import settings
 
-router = routers.DefaultRouter()
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
